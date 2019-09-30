8f4229a378f9cc9d4fdd3f769bd2a32677acc9ad
Mark Handley <mark@handley.org.uk>
2019-09-30 23:15:15 +0100

    Add slides

---
+++ Topics/01_Basics/src/hello_world.py
@@ -0,0 +1,8 @@
+# File: hello_world.py 
+
+def hello_world():
+    print("Hello World!") 
+
+# run if called as `python hello_world.py'
+if __name__ == "__main__": # Python idiom
+    hello_world()          # `Call' the function `hello_world'
